82f35f320ef7100ba86cc80a1f0290e2a6f34b44
nevya <nevyadomsky7@gmail.com>
2026-07-15 14:18:39 +0200

    Auto-update via script

--- lesson18(dict)(робота з даними).py
+++ lesson18(dict)(робота з даними).py
@@ -133,5 +133,6 @@ def find_expensive_product(products):
     for product in products:
         if expensive_product is None:
             expensive_product = product
+        elif
     return product
 print(find_expensive_product(products))
