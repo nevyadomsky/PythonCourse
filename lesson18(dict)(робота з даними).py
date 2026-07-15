349c81d63f0e975f8bde8b320f0f84d88dc73a3f
nevya <nevyadomsky7@gmail.com>
2026-07-15 11:57:59 +0200

    Auto-update via script

--- lesson18(dict)(робота з даними).py
+++ lesson18(dict)(робота з даними).py
@@ -16,14 +16,14 @@
 #    }        # Пари: ключ → значення
 
 
-def create_user(name, age):
-    user = {
-        "name": name,
-        "age": age
-    }
-    return user
-user = create_user("Anna", 20)
-print(user)
+# def create_user(name, age):
+#     user = {
+#         "name": name,
+#         "age": age
+#     }
+#     return user
+# user = create_user("Anna", 20)
+# print(user)
 
 
 def create_user(name, age):
