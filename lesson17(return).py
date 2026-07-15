5e1ddc09a6f1ab09d8c05e8b7d431abf0112032c
nevya <nevyadomsky7@gmail.com>
2026-07-15 11:08:37 +0200

    Auto-update via script

--- lesson17(return).py
+++ lesson17(return).py
@@ -107,7 +107,7 @@
 def biggest(numbers):
     num = 0
     for number in numbers:
-        if number > number:
+        if num > number:
             num = number + 1
     return num
 numbers = [15, 7, 22, 9, 18]
