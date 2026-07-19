7fc236d1c9880b0a6bd52e13d7b08515874b1366
nevya <nevyadomsky7@gmail.com>
2026-07-19 17:42:22 +0200

    Meine Übungen

---
+++ project01.py
@@ -0,0 +1,13 @@
+number1 = int(input("Enter a number 1: "))
+number2 = int(input("Enter a number 2: "))
+operation = input("Enter operation: ")
+
+if operation == "+":
+    print(number1 + number2)
+elif operation == "-":
+    print(number1 - number2)
+elif operation == "*":
+    print(number1 * number2)
+elif operation == "/":
+    print(number1 / number2)
+
